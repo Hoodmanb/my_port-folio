f271c5b73b9dc6df9ec5ad32d936e685d158cb72
hoodmanb <joshuadebravo@gmail.com>
2023-11-11 16:21:14 +0100

    first commit (worked on the index page)

---
+++ js-files/index.js
@@ -0,0 +1,5 @@
+  // Toggle the display of the dropdown options
+  function toggleDropdown() {
+    var dropdownOptions = document.getElementById("dropdownOptions");
+    dropdownOptions.classList.toggle("show");
+}
