34b5476dd213ed73c1f86104fd1235737c4d0ee0
hoodmanb <joshuadebravo@gmail.com>
2023-12-12 14:42:39 +0100

    update

--- js-files/index.js
+++ js-files/index.js
@@ -2,4 +2,26 @@
 function dropdown() {
     let dropDownOptions = document.getElementById("options");
     dropDownOptions.classList.toggle("show");
-}
+};
+
+
+
+//zoomout for the tools-logo
+const zoomable = document.getElementsByClassName("tools-logo");
+
+zoomable.forEach(zoom => {
+    zoom.addEventListener('click', () => {
+        zoomable.forEach(el => el.classList.remove('zoomed'));
+        zoom.classList.add('zoomed');
+    });
+});
+
+
+
+/*let toolsLogo = document.getElementsByClassName('tools-logo')
+
+function zoom(){
+    toolsLogo.classList.add('zoomed')
+    console.log("my name is Joshua");
+};
+*/
